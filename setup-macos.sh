8e01a15077b985576266f2074bd922efda743fff
Chaudhry Junaid Anwar <junaidanwar10@gmail.com>
2020-12-08 18:52:04 +0500

    update initial script

--- setup-macos.sh
+++ setup-macos.sh
@@ -6,38 +6,67 @@ echo "Setting up your mac using chaudhryjunaid/setup-macos"
 echo "This scrips installs pre-requisites and runs ansible"
 echo "===================================================="
 
-# Check if console tools installed
+# Ensure command line tools are installed
 if ! xcode-select -p >/dev/null; then
+    echo "Installing XCode Command-line Tools..."
 	xcode-select --install
-	read -p "Press [Enter] when install finished..."
+	read -p "Press [Enter] when Command-line Tools have been installed..."
+else
+    echo "✓ XCode Command-line tools are already installed"
 fi
 
 if ! xcode-select -p >/dev/null; then
     echo "Command-line tools could not be installed. Please try running this script again!"
 fi
 
+if ! hash brew 2>/dev/null; then
+    echo "Installing Homebrew..."
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+else
+    echo "✓ Homebrew is already installed"
+fi
+
+if ! hash brew 2>/dev/null; then
+    echo "Homebrew could not be installed. Please try running this script again!"
+fi
+
+echo "Updating brew..."
+brew update
+
 if ! hash ansible 2>/dev/null; then
+    echo "Installing ansible..."
     brew install ansible
+else
+    echo "✓ Ansible is already installed"
+fi
+
 fi
 
 if ! hash ansible 2>/dev/null; then
     echo "Ansible could not be installed. Please try running this script again!"
 fi
 
-installdir=~/tmp/setup-macos
+installdir=~/macos-setup
+echo "Checking if $installdir exists..."
+
 if [ ! -d $installdir ]; then
+    echo "Creating $installdir and cloning setup git repo in it..."
     mkdir -p $installdir
     git clone https://github.com/chaudhryjunaid/setup-macos.git $installdir
+else
+    echo "✓ $installdir exists"
 fi
 
-if [ ! -d "${installdir}/.git" ]; then
-    echo "Failed to find setup-macos (git clone failed)."
+if [ ! -f "${installdir}/macos-setup.yml"]; then
+    echo "Failed to find setup-macos (probably git clone failed or directory already contained something else)."
     exit 1
+else
+    echo "✓ Playbook exists"
 fi
 
+echo "Starting playbook..."
 cd $installdir 
-ansible-playbook macos-setup.yml --verbose
-
-echo "MacOS setup is now complete! Enjoy!"
+#time ansible-playbook macos-setup.yml --verbose
+echo "Ansible finished. See  logs for status."
 
 exit 0
