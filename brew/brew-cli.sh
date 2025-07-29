1856bf1f9a0bae8fc87293ba3835b4de212e049d
Chaudhry Junaid Anwar <junaidanwar10@gmail.com>
2025-07-29 19:37:23 +0500

    update brew-cli.sh

--- brew/brew-cli.sh
+++ brew/brew-cli.sh
@@ -2,24 +2,26 @@
 brew install zsh
 brew install vim
 brew install git
+brew install git-delta
 brew install gh
 brew install liquidprompt
 brew install fzf
+# brew install bat
+# brew install fd
+# brew install zoxide
 brew install ripgrep
 brew install tmux
 brew install tmuxinator
+# brew install screen
 
 # system utils
-brew install inetutils
 brew install coreutils
 brew install findutils
-brew install screen
+brew install inetutils
 
 # development tools
 brew install ansible
 brew install awscli
-brew install aria2
-brew install wget
 
 # cli utils
 brew install tree
@@ -27,9 +29,10 @@ brew install jq
 #brew install yq
 brew install htop
 brew install cloc
-brew install git-delta
 #brew install glances
 brew install procs
+brew install aria2
+brew install wget
 
 # specialized tools
 #brew install ncdu
