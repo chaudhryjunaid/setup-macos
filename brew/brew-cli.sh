d2b7c06201af6077007d557c83b45a8cb123d2c4
Chaudhry Junaid Anwar <junaidanwar10@gmail.com>
2025-08-09 22:45:36 +0500

    update brew-cli pkgs

--- brew/brew-cli.sh
+++ brew/brew-cli.sh
@@ -1,4 +1,4 @@
-# basics
+# terminal basics
 brew install zsh
 brew install vim
 brew install git
@@ -7,13 +7,18 @@ brew install gh
 # brew install liquidprompt
 brew install starship
 brew install fzf
-# brew install bat
+brew install bat
 # brew install fd
 # brew install zoxide
 brew install ripgrep
 brew install tmux
 brew install tmuxinator
+brew install tmuxinator-completion
 # brew install screen
+
+# cli apps
+breww install lazygit
+brew install tig
 
 # system utils
 brew install coreutils
@@ -24,15 +29,15 @@ brew install inetutils
 brew install ansible
 brew install awscli
 
-# cli utils
+# cli tools
 brew install tree
 brew install jq
 #brew install yq
 brew install htop
 brew install cloc
 brew install procs
-brew install aria2
-brew install wget
+# brew install aria2
+# brew install wget
 
 # specialized tools
 #brew install ncdu
